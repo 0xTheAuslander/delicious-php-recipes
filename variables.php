34465981e437f573f6bddecaa88ccf6c6a037558
0xAuslander_ <35664385+0xTheAuslander@users.noreply.github.com>
2024-02-28 09:26:38 +0100

    Add initial php code for capturing user inputs via contact form

---
+++ variables.php
@@ -0,0 +1,50 @@
+<?php 
+
+$recipes = [
+    [
+        'title' => 'Salade romaine',
+        'recipe' => 'Etape 1 : des onions !',
+        'author' => 'mickael.qwe@exemple.com',
+        'is_enabled' => true,
+    ],
+    [
+        'title' => 'Couscous',
+        'recipe' => 'Etape 1 : de la semoule',
+        'author' => 'mick.yzx@exemple.com',
+        'is_enabled' => true,
+    ],
+    [
+        'title' => 'Escalope milanaise',
+        'recipe' => 'Etape 1 : prenez une belle escalope',
+        'author' => 'mathieu.xyz@exemple.com',
+        'is_enabled' => false,
+    ],
+];
+
+$users = [
+    [
+        'full_name' => 'Mickael',
+        'age' => 34,
+        'email' => 'mickael.qwe@exemple.com',
+    ],
+    [
+        'full_name' => 'Mick',
+        'age' => 24,
+        'email' => 'mick.yzx@exemple.com',
+    ],
+    [
+        'full_name' => 'Mathieu',
+        'age' => 44,
+        'email' => 'mathieu.xyz@exemple.com',
+    ],
+    [
+        'full_name' => 'Mourad',
+        'age' => 18,
+        'email' => 'mourad.xyz@exemple.com',
+    ],
+    [
+        'full_name' => 'Said',
+        'age' => 58,
+        'email' => 'said.xyz@exemple.com',
+    ],
+];
